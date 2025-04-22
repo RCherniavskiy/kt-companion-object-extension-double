9a0dc2c0c0c951bdec997f0ecbdb2059b1171c9a
Ruslan Cherniavskyi <ruslan.od.ua@ukr.net>
2025-04-22 10:37:37 +0300

    solved task

--- src/main/kotlin/mate/academy/DoubleCompanionExtension.kt
+++ src/main/kotlin/mate/academy/DoubleCompanionExtension.kt
@@ -1,3 +1,10 @@
 package mate.academy
 
-// provide your solution below
+fun Double.Companion.printClassName() {
+    print("Class name: ${this::class.qualifiedName}" + "\n")
+}
+
+fun main() {
+    Double.printClassName()
+}
+
